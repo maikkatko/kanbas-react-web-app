b35f8e7115af7f8b81f4dd76bcb71a4150d682e3
maikkatko <maikkatko@gmail.com>
2024-05-16 20:41:15 -0400

    Section 3.7 work

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -4,8 +4,9 @@ export default function AssignmentEditor() {
       <label htmlFor="wd-name">Assignment Name</label>
       <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
       <textarea id="wd-description">
-        The assignment is available online Submit a link to the landing page of
+        The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
       </textarea>
+      <br />
       <br />
       <table>
         <tr>
@@ -16,7 +17,80 @@ export default function AssignmentEditor() {
             <input id="wd-points" value={100} />
           </td>
         </tr>
-        {/* Complete on your own */}
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-group">Assignment Group</label>
+          </td>
+          <td>
+            <select id="wd-group">
+              <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-display-grade-as">Display Grade as</label>
+          </td>
+          <td>
+            <select id="wd-display-grade-as">
+              <option value="PERCENTAGE">Percentage</option>
+              <option value="POINTS">Points</option>
+            </select>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-submission-type">Submission Type</label>
+          </td>
+          <td>
+            <select id="wd-submission-type">
+              <option value="ONLINE">Online</option>
+            </select>
+            <br />
+            <br />
+            <label>Online Entry Options</label>
+            <br />
+            <input type="checkbox"
+              name="check-options" id="wd-text-entry" />
+            <label htmlFor="wd-text-entry">Text Entry</label><br />
+            <input type="checkbox"
+              name="check-options" id="wd-website-url	" />
+            <label htmlFor="wd-website-url">Website URL</label><br />
+            <input type="checkbox"
+              name="check-options" id="wd-media-recordings" />
+            <label htmlFor="wd-media-recordings">Media Recordings</label><br />
+            <input type="checkbox"
+              name="check-options" id="wd-student-annotation" />
+            <label htmlFor="wd-student-annotation">Student Annotation</label><br />
+            <input type="checkbox"
+              name="check-options" id="wd-file-upload" />
+            <label htmlFor="wd-file-upload">File Upload</label>
+          </td>
+        </tr>
+        <br />
+        <tr>
+          <td align="right" valign="top">
+            <label htmlFor="wd-assign-to">Assign</label>
+          </td>
+          <td>
+            <label>Assign To</label><br />
+            <input id="wd-assign-to" value={"Everyone"} />
+            <br /><br />
+            <label htmlFor="wd-due-date">Due Date</label><br />
+            <input type="date" id="wd-due-date" value={"2024-05-13"} />
+            <br /><br />
+            <td>
+              <label htmlFor="wd-available-from">Available from</label><br />
+              <input type="date" id="wd-available-from" value={"2024-05-06"} />
+            </td>
+            <td>
+              <label htmlFor="wd-available-until">Until</label><br />
+              <input type="date" id="wd-available-until" value={"2024-05-20"} /></td>
+          </td>
+        </tr>
       </table>
     </div>
   );
