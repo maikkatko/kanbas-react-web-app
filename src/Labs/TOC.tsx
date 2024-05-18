007172bb28ba2fb35dc46a3b58031795ddf13015
maikkatko <maikkatko@gmail.com>
2024-05-18 15:51:27 -0400

    Fixed repo link label again

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -6,7 +6,7 @@ export default function TOC() {
       <li><a id="wd-a2" href="#/Labs/Lab2">Lab 2</a></li>
       <li><a id="wd-a3" href="#/Labs/Lab3">Lab 3</a></li>
       <li><a id="wd-k" href="#/Kanbas">Kanbas</a></li>
-      <li><a id="wd-github" href="https://github.com/maikkatko/kanbas-react-web-app/tree/a1">GitHub Reo</a></li>
+      <li><a id="wd-github" href="https://github.com/maikkatko/kanbas-react-web-app/tree/a1">GitHub Repo</a></li>
     </ul>
   );
 }
