8fa390474fd515fec4ead5d5a2add5393a661332
maikkatko <maikkatko@gmail.com>
2024-05-22 20:42:01 -0400

    Updated Editor tto make editable field values default so that they can be updated, and added lots of whitespace to move cancel and save buttons to the right to match the hw

--- src/Kanbas/Courses/Assignments/Editor.tsx
+++ src/Kanbas/Courses/Assignments/Editor.tsx
@@ -2,7 +2,7 @@ export default function AssignmentEditor() {
   return (
     <div id="wd-assignments-editor">
       <label htmlFor="wd-name">Assignment Name</label>
-      <input id="wd-name" value="A1 - ENV + HTML" /><br /><br />
+      <input id="wd-name" defaultValue="A1 - ENV + HTML" /><br /><br />
       <textarea id="wd-description">
         The assignment is available online Submit a link to the landing page of your Web application running on Netlify. The landing page should include the following: Your full name and section Links to each of the lab assignments Link to the Kanbas application Links to all relevant source code repositories The Kanbas application should include a link to navigate back to the landing page.
       </textarea>
@@ -14,7 +14,7 @@ export default function AssignmentEditor() {
             <label htmlFor="wd-points">Points</label>
           </td>
           <td>
-            <input id="wd-points" value={100} />
+            <input id="wd-points" defaultValue={100} />
           </td>
         </tr>
         <br />
@@ -79,26 +79,33 @@ export default function AssignmentEditor() {
           </td>
           <td>
             <label>Assign To</label><br />
-            <input id="wd-assign-to" value={"Everyone"} />
+            <input id="wd-assign-to" defaultValue={"Everyone"} />
             <br /><br />
             <label htmlFor="wd-due-date">Due Date</label><br />
-            <input type="date" id="wd-due-date" value={"2024-05-13"} />
+            <input type="date" id="wd-due-date" defaultValue={"2024-05-13"} />
             <br /><br />
             <td>
               <label htmlFor="wd-available-from">Available from</label><br />
-              <input type="date" id="wd-available-from" value={"2024-05-06"} />
+              <input type="date" id="wd-available-from" defaultValue={"2024-05-06"} />
             </td>
             <td>
               <label htmlFor="wd-available-until">Until</label><br />
-              <input type="date" id="wd-available-until" value={"2024-05-20"} /></td>
+              <input type="date" id="wd-available-until" defaultValue={"2024-05-20"} /></td>
           </td>
         </tr>
       </table>
       <hr />
-      <div>
-        <button id="wd-cancel" type="button">Cancel</button>
-        <button id="wd-save" type="button">Save</button>
-      </div>
+      <table>
+        <tr>
+          <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+          <td>
+            <button id="wd-cancel" type="button">Cancel</button>
+          </td>
+          <td>
+            <button id="wd-save" type="button">Save</button>
+          </td>
+        </tr>
+      </table>
     </div>
   );
 }
