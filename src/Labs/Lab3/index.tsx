59031435406e1b0e260f6d074e2f47c0274b0b6b
maikkatko <maikkatko@gmail.com>
2024-06-21 20:31:03 -0400

    Finished lab

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,12 +31,22 @@ import TernaryOperator from "./TernaryOperator";
 import VariableTypes from "./VariableTypes";
 import VariablesAndConstants from "./VariablesAndConstants";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
   console.log("Hello World")
+  const { todos } = useSelector((state: any) => state.todosReducer);
   return (
     <div id="wd-lab3" className="container-fluid">
       <h3>JavaScript</h3>
+      <ul className="list-group">
+        {todos.map((todo: any) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
