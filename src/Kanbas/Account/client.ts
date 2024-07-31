5a4d4cc2bcb18ea4885cc41a50a3b09813b8c94f
maikkatko <maikkatko@gmail.com>
2024-07-31 11:38:43 -0400

    Troubleshoot done, update to remove debugs

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -3,8 +3,6 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 export const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export const USERS_API = `${REMOTE_SERVER}/api/users`;
 
-console.log("Remote Server: ", REMOTE_SERVER);
-
 export const signin = async (credentials: any) => {
   const response = await axiosWithCredentials.post(`${USERS_API}/signin`, credentials);
   return response.data;
